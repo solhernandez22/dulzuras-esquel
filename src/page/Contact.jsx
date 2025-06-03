bce43ed894182acdf7f184f505abb8acfa743e50
ronywin <ronyewinter@gmail.com>
2025-06-02 21:17:03 -0300

    reinicio de plantilla

--- src/page/Contact.jsx
+++ src/page/Contact.jsx
@@ -1,12 +1,18 @@
 function Contact() {
   return (
     <div>
-      <h1 class="titulos">Contact</h1>
-      <p>Contact page:
-        2945- 666
+      <h1>Contacto</h1>
+      <p>
+        Si tienes alguna pregunta o deseas realizar un pedido, no dudes en
+        contactarnos. Estamos aquí para ayudarte a disfrutar de nuestras
+        deliciosas creaciones.
+      
+      
+        Puedes llamarnos al <strong>(123) 456-7890</strong> o enviarnos un
+        correo electrónico a <strong>777@falso.com</strong>
       </p>
     </div>
-  );
-}
+    )
+    }
 
 export default Contact;
--- src/page/Galeria.jsx
+++ src/page/Galeria.jsx
@@ -1,18 +1,8 @@
-import CircularGallery from './CircularGallery.jsx';
-
-
 function Galeria() {
   return (
     <div>
-      <h1 className="text-black">Galeria de Trunks</h1>
+      <h1>CAMPO DE TEXTO</h1>
       
-      <p>a</p>
-      <div style={{ height: '600px', position: 'relative' }}>
-  <CircularGallery bend={3} textColor="#ffffff" borderRadius={0.05} />
-</div>
-<div style={{ height: '600px', position: 'relative' }}>
-  <CircularGallery bend={3} textColor="#ffffff" borderRadius={0.05} />
-</div>
     </div>
     
     )}
