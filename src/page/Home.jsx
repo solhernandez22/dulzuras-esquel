9db8c43cd3fe0168fe1cebb75df6b19f5686f069
ronywin <ronyewinter@gmail.com>
2025-06-03 19:53:58 -0300

    homecss

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,6 +1,9 @@
+import './Home.css'
+
+
 function Home () {
   return (
-    <div>
+    <div classname= "divHome">
       <h1>Bienvenidos a Dulzuras Esquel</h1>
       <p>
         Descubre la magia de la repostería artesanal en el corazón de Esquel.
