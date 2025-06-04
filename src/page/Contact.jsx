e26934d96a727a7fa15f780d2b019c9ac34f09d5
solhernandez22 <solangeivonnehernandez@gmail.com>
2025-06-03 21:54:50 -0300

    pagina contacto

--- src/page/Contact.jsx
+++ src/page/Contact.jsx
@@ -1,20 +1,26 @@
 import './Contacto.css'
-
+import { FaMapLocation } from "react-icons/fa6";
 
 
 function Contact() {
   return (
-    <div className='contacto'>
-      <h1>Contacto</h1>
+    <div  className="fondou">
+      <div>
+      <h1>INFORMACION DE CONTACTO</h1>
       <p>
         Si tienes alguna pregunta o deseas realizar un pedido, no dudes en
         contactarnos. Estamos aquí para ayudarte a disfrutar de nuestras
         deliciosas creaciones.
-      
+      </p>
+  <a href='https://maps.app.goo.gl/YX9inR3VBwqL7y5K9'><FaMapLocation /></a>
+  
+    
+      <p>
       
         Puedes llamarnos al <strong>(123) 456-7890</strong> o enviarnos un
         correo electrónico a <strong>777@falso.com</strong>
       </p>
+      </div>
     </div>
     )
     }
