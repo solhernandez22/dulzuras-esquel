53e269ec855143ebd3d8b663fdd14c8ad2499557
solhernandez22 <solangeivonnehernandez@gmail.com>
2025-06-06 21:37:59 -0300

    modificaciones

---
+++ src/Menu.js
@@ -0,0 +1,8 @@
+export const menu = [
+  {
+    Image: 'https://scontent.fcrd6-1.fna.fbcdn.net/v/t51.75761-15/465257210_18273668281216366_3693589318178647075_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=127cfc&_nc_ohc=2QSb3fHuNg0Q7kNvwEXrRoT&_nc_oc=AdkzboAaO1bQaqL63wOTD5VyPzzyTj2h54CKS629pbPNAcmRn8Crn0C8j11C38RfwXo&_nc_zt=23&_nc_ht=scontent.fcrd6-1.fna&_nc_gid=Dgnr3We-OHqbv9JUUWzmNw&oh=00_AfOI6z0vnF7DXjTTGiZe1nP8bshHeudvKV8DrteAR3s-mg&oe=6849555E',
+    Name: 'Lemon Pie',
+    Description: 'Delicioso pie de limón con base de galleta y merengue.',
+    Price: 2500
+  }
+];
--- src/page/About.jsx
+++ src/page/About.jsx
@@ -1,17 +1,22 @@
-function about() {
+import { menu } from '../menu.js';
+import './Menu.css'
+
+function About() {
   return (
-    <div className="about">
-      <h1>Sobre Dulzuras Esquel</h1>
-      <p>
-        Dulzuras Esquel es una empresa familiar dedicada a la elaboración de
-        productos de repostería y panadería. Con más de 20 años de experiencia,
-        ofrecemos una amplia variedad de productos frescos y deliciosos.
-      </p>
-      <p>
-        Nuestro compromiso es brindar calidad y sabor en cada uno de nuestros
-        productos, utilizando ingredientes frescos y naturales.
-      </p>
+    <div className="fondo">
+      
+      <h1>Nuestro Menú</h1>
+      <div>
+        {menu.map((item, idx) => (
+          <div key={idx}>
+            <img src={item.Image} alt={item.Name} width={150} />
+            <h3>{item.Name}</h3>
+            <p>{item.Description}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
-export default about;
+
+export default About;
