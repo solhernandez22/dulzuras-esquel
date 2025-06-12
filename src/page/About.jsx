319b95dd5388a3da26f9be2ddac510c2a19e98e3
solhernandez22 <solangeivonnehernandez@gmail.com>
2025-06-11 21:51:18 -0300

    menu terminado

--- src/page/About.jsx
+++ src/page/About.jsx
@@ -6,19 +6,38 @@ function About() {
     <div className="fondo">
       
       <h1 className='titulo'>Nuestro Menú</h1>
-      <p className='parrafo'>En nuestra cafetería, ofrecemos una variedad de delicias para satisfacer todos los gustos. Desde el clásico café recién hecho hasta el reconfortante chocolate caliente, cada uno de nuestros productos está elaborado con ingredientes frescos y de alta calidad. ¡Ven a disfrutar de un momento especial con nosotros!</p>
+      <h2>Bebidas Calientes</h2>
       <div className='comida'>
-        {menu.map((item, idx) => (
-          <div className='tarjeta'>
-          <div key={idx}>
+        {menu.filter(item => item.Category === 'Bebidas Calientes').map((item, idx) => (
+          <div className='tarjeta' key={idx}>
             <img src={item.Image} alt={item.Name} width={150} />
             <h3>{item.Name}</h3>
             <p>{item.Description}</p>
-            <p>{item.Price}</p>
-          </div>
+            <p>Precio: ${item.Price}</p>
           </div>
         ))}
-        
+      </div>
+      <h2>Bebidas Frías</h2>
+      <div className='comida'>
+        {menu.filter(item => item.Category === 'Bebidas Frías').map((item, idx) => (
+          <div className='tarjeta' key={idx}>
+            <img src={item.Image} alt={item.Name} width={150} />
+            <h3>{item.Name}</h3>
+            <p>{item.Description}</p>
+            <p>Precio: ${item.Price}</p>
+          </div>
+        ))}
+      </div>
+            <h2>Pasteleria</h2>
+      <div className='comida'>
+        {menu.filter(item => item.Category === 'Pasteleria').map((item, idx) => (
+          <div className='tarjeta' key={idx}>
+            <img src={item.Image} alt={item.Name} width={150} />
+            <h3>{item.Name}</h3>
+            <p>{item.Description}</p>
+            <p>Precio: ${item.Price}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
